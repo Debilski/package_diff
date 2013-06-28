a0ed427d6648e4f36e8af820d43e82d9e7d95b3e
Rike-Benjamin Schuppner <rikebs@debilski.de>
2013-06-28 11:43:22 +0200

    ENH: Show the state of the package if it’s not ‘ii’.

--- package_diff.py
+++ package_diff.py
@@ -11,11 +11,11 @@ def get_installed_packages(host, user=None, pw=None):
     cmd = ['ssh', h, "dpkg -l"]
     exe = Popen(cmd, stdout=PIPE, stderr=PIPE)
     p = {}
-    pkgs = exe.stdout.read().split("\n")
+    pkgs = exe.stdout.readlines()[4:]
     for pkg in pkgs:
         pkg_split = pkg.split()
         try:
-            p[pkg_split[1]] = pkg_split[2]
+            p[pkg_split[1]] = (pkg_split[2], pkg_split[0])
         except:
             pass
     return p
@@ -30,15 +30,20 @@ if __name__ == "__main__":
 
     for host in hosts:
         packages = get_installed_packages(host)
-        for (pkg, version) in packages.iteritems():
+        for (pkg, version_state) in packages.iteritems():
+            version, state = version_state
+            if state != "ii":
+                host_ = "{}({})".format(host, state)
+            else:
+                host_ = host
             global_count[pkg] += 1
             global_count_v[pkg + "-" + version] += 1
-            occurence[pkg].append(host)
-            occurence_v[pkg + "-" + version].append(host)
+            occurence[pkg].append(host_)
+            occurence_v[pkg + "-" + version].append(host_)
 
     for (pkg, count) in sorted(global_count.items(), key=lambda item: - item[1]):
-        print "{:>3d} {:<16s} {}".format(count, pkg[:16], occurence[pkg])
+        print "{:>3d} {:<16s} [ {} ]".format(count, pkg[:16], " ".join(occurence[pkg]))
 
     for (pkg, count) in sorted(global_count_v.items(), key=lambda item: - item[1]):
-        print "{:>3d} {:<26s} {}".format(count, pkg[:26], occurence_v[pkg])
+        print "{:>3d} {:<26s} [ {} ]".format(count, pkg[:26], " ".join(occurence_v[pkg]))
 
